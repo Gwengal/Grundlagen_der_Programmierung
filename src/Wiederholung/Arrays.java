db0a269f080b4b837762a0c1bc3a8fd09fa07eca
Gwengal <alexanderkaiser93@yahoo.com>
2023-11-16 11:15:18 +0100

    GDP-Theorie - 16.11.2023

--- src/Wiederholung/Arrays.java
+++ src/Wiederholung/Arrays.java
@@ -7,6 +7,8 @@ public class Arrays {
     public static void main(String[] args) {
         int[] aInt = {1, 2, 3, 4, 5};
         Arrays.printArray(aInt);
+        Arrays.addOne(aInt);
+        Arrays.printArray(aInt);
     }
 
     /**
@@ -16,5 +18,15 @@ public class Arrays {
         for (int i = 0; i < aInt.length; i++) {
             System.out.print(aInt[i] + " ");
         }
+    }
+
+    /**
+     * @param aInt
+     */
+    private static void addOne(int[] aInt){
+        for (int i = 0; i < aInt.length; i++) {
+            aInt[i] += 1;
+        }
+        System.out.println();
     }
 }
