17f7ab726457cac895be66bd3009067f530a46db
Gwengal <alexanderkaiser93@yahoo.com>
2023-11-21 19:29:34 +0100

    Testat 6 - Aufgabe 8

--- src/Testate/TicTacToe.java
+++ src/Testate/TicTacToe.java
@@ -15,9 +15,6 @@ public class TicTacToe {
 
         // --| TicTacToe ausführen |--
         while (true) {
-            // --| Spielfeld initialiseren und ausgeben |--
-            TicTacToe.printArray2(aPlayfield, true);
-
             // --| TicTacToe starten |--
             TicTacToe.startGame(aPlayfield, oScanner);
 
@@ -34,51 +31,110 @@ public class TicTacToe {
 
             // Schleife verlassen und Logik beenden
             break;
-            
         }
     }
 
     /**
+     * TicTacToe-Spiel starten
+     * 1. Spielfeld initialisieren und ausgeben
+     * 2. Abfragen ob der Anwender gegen den Rechner spielen möchte
+     * 3. Spielernamen auslesen
+     * 4. Eingaben auslesen und validieren 
+     * 5. Rechner Koordinaten berechnen lassen
+     * 6. Koordinaten setzen
+     * 7. Spielfeld ausgeben
+     * 8. Prüfen ob der aktuelle Spieler gewonnen hat
+     * 
      * @param aPlayfield
-     * @param aCoordinate
      * @param oScanner
      */
     private static void startGame(char[][] aPlayfield, Scanner oScanner) {
         // --| Deklaration |--
         char cTurnChar;
-        String sPlayer;
-        
+        boolean bPCTurn = false,
+                bPlayAgainstPC = false;
+        String sInput,
+               sCurrPlayer,
+               sPlayer1,
+               sPlayer2 = "PC";
+
+        // --| Spielfeld initialiseren und ausgeben |--
+        TicTacToe.printArray2(aPlayfield, true);
+
         /*
-         * 1. Ermittlung um welchen Spieler es sich in der aktuellen Runde handelt
-         * 2. Spielzug des Spieler durchführen
-         * 2.1 Eingabe aus Terminal auslesen
-         * 2.2 Eingabe validieren
-         * 2.3 Koordinaten setzen
-         * 3. Geändertes Spielfeld ausgeben
-         * 4. Prüfen ob ein Spieler gewonnen hat oder ob Unentschieden ist
+         * Abfragen ob der User gegen den Rechner spielen möchte oder
+         * gegen einen weiteren "echten" Menschen
          */
+        System.out.print("Wollen Sie gegen den Rechner spielen [y/Y]?");
+        sInput = oScanner.nextLine();
+        bPlayAgainstPC = (sInput.contains("y") || sInput.contains("Y") ? true : false);
+
+        // Namen für Spieler 1 auslesen
+        System.out.print("Erfassen Sie den Namen des ersten Spielers: ");
+        sPlayer1 = oScanner.nextLine();
+
+        /*
+         * Es wird nicht gegen den Rechner gespielt
+         * -> Namen für den zweiten Spieler auslesen
+         */
+        if (!bPlayAgainstPC) {
+            System.out.print("Erfassen Sie den Namen für den zweiten Spieler: ");
+            sPlayer2 = oScanner.nextLine();
+        }
+
+        // --| Ein Spiel kann max. 9 Runden dauern (3x3) |--
         for (int i = 0; i < 9; i++) {
+            /*
+             * 1. Setzen des aktuellen Spielers + dessen Spielzeichen
+             * 2. Setzen ob der Rechner an der Reihe ist
+             */
             if (i % 2 == 0) {
-                sPlayer = "Spieler 1";
+                sCurrPlayer = sPlayer1;
                 cTurnChar = 'x';
-            }else{
-                sPlayer = "Spieler 2";
+                bPCTurn = false;
+            } else {
+                sCurrPlayer = sPlayer2;
                 cTurnChar = 'o';
+                bPCTurn = bPlayAgainstPC;
             }
 
-            TicTacToe.setTurn(sPlayer, cTurnChar, aPlayfield, oScanner);
+            /*
+             * 1. User ist am Zug
+             * 3.1 Eingabe aus der Konsole auslesen
+             * 3.2 Eingabe validieren
+             * 3.3 Koordinaten belegen
+             * 2. Rechner ist am Zug
+             * 2.1 ToDo
+             */
+            TicTacToe.setTurn(bPCTurn, sCurrPlayer, cTurnChar, aPlayfield, oScanner);
+            // Spielfeld ausgeben
             TicTacToe.printArray2(aPlayfield, false);
-            
+
             // --| Prüfen ob der aktuelle Spieler gewonnen hat |--
-            if (TicTacToe.checkWin(cTurnChar, sPlayer, aPlayfield)) {
+            if (TicTacToe.checkWin(cTurnChar, sCurrPlayer, aPlayfield)) {
                 break;
             }
 
             // --| Unentschieden, keiner hat verloren |--
-            if ((i+1) == 9) {
+            if ((i + 1) == 9) {
                 System.out.println("Unentschieden!");
             }
         }
+    }
+
+    /**
+     * ToDo
+     * 
+     * @param cTurnChar
+     * @param sPlayer
+     * @param aPlayfield
+     * @return
+     */
+    private static int[] pcTurn(String sPlayer, char[][] aPlayfield) {
+
+
+
+        return null;
     }
 
     /**
@@ -104,7 +160,7 @@ public class TicTacToe {
                 bWin = true;
                 sWinOutput = sWinOutput + " horizontal gewonnen!";
                 break;
-            }          
+            }
 
             // Vertikal gewonnen
             if (aPlayfield[0][i] == cTurnChar && aPlayfield[1][i] == cTurnChar && aPlayfield[2][i] == cTurnChar) {
@@ -115,7 +171,8 @@ public class TicTacToe {
         }
 
         // Schräg gewonnen
-        if ((aPlayfield[0][0] == cTurnChar && aPlayfield[1][1] == cTurnChar && aPlayfield[2][2] == cTurnChar) || (aPlayfield[0][2] == cTurnChar && aPlayfield[1][1] == cTurnChar && aPlayfield[2][0] == cTurnChar)) {
+        if ((aPlayfield[0][0] == cTurnChar && aPlayfield[1][1] == cTurnChar && aPlayfield[2][2] == cTurnChar)
+                || (aPlayfield[0][2] == cTurnChar && aPlayfield[1][1] == cTurnChar && aPlayfield[2][0] == cTurnChar)) {
             bWin = true;
             sWinOutput = sWinOutput + " schräg gewonnen!";
         }
@@ -124,26 +181,33 @@ public class TicTacToe {
         if (bWin) {
             System.out.println(sWinOutput);
         }
-        
+
         return bWin;
     }
 
     /**
-     * 1. Eingabe aus der Konsole auslesen
-     * 2. Eingabe validieren
-     * 3. Koordinaten setzen
+     * ToDo
      * 
-     * @param iRound
+     * @param bPCTurn
+     * @param sPlayer
+     * @param cTurnChar
      * @param aPlayfield
      * @param aCoordinate
      * @param oScanner
      */
-    private static void setTurn(String sPlayer, char cTurnChar, char[][] aPlayfield, Scanner oScanner) {
+    private static void setTurn(boolean bPCTurn, String sPlayer, char cTurnChar, char[][] aPlayfield,
+            Scanner oScanner) {
         // --| Deklaration |--
         int[] aCoordinate;
 
         // --| Eingabe aus der Konsole auslesen und validieren |--
-        aCoordinate = TicTacToe.getInput(sPlayer, aPlayfield, oScanner);
+        if (!bPCTurn) {
+            aCoordinate = TicTacToe.getInput(sPlayer, aPlayfield, oScanner);
+        }
+        // --| PC hat sich Koordinaten ausgewählt |--
+        else {
+            aCoordinate = TicTacToe.pcTurn(sPlayer, aPlayfield);
+        }
 
         // Koordinaten setzen
         aPlayfield[(aCoordinate[0] - 1)][(aCoordinate[1] - 1)] = cTurnChar;
@@ -213,12 +277,12 @@ public class TicTacToe {
      */
     private static void validateTurn(char[][] aPlayfield, int[] aCoordinate) throws Exception {
         // Zeileneingabe liegt nicht im Wertebereich 1-3
-        if (aCoordinate[0] < 0 || aCoordinate[0] > 3) {
+        if (aCoordinate[0] <= 0 || aCoordinate[0] > 3) {
             throw new Exception("Eingabe Zeile (Wert: " + aCoordinate[0] + ") liegt nicht im Bereich 1-3.");
         }
 
         // Spalteneingabe liegt nicht im Wertebereich 1-3
-        if (aCoordinate[1] < 0 || aCoordinate[1] > 3) {
+        if (aCoordinate[1] <= 0 || aCoordinate[1] > 3) {
             throw new Exception("Eingabe Spalte (Wert: " + aCoordinate[1] + ") liegt nicht im Bereich 1-3.");
         }
 
